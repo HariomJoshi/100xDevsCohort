3fa1645f6c44273c3bb567d140650a351aec3940
Hariom Joshi <goodhariom@gmail.com>
2024-01-11 20:25:12 +0530

    react foundations

---
+++ week5/button-app/src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import { useState } from "react";
+
+// todo app
+// state
+//{
+//  todos:[{title:"first", description:"complete by 10 jan", completed:false,}]
+//}
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      Title: "Go to gym",
+      Description: "Daily exercise",
+      completed: "false",
+    },
+    {
+      Title: "study DSA",
+      Description: "dynamic programming tabulation",
+      completed: "false",
+    },
+  ]);
+  // this is our initial state
+
+  return (
+    <div>
+      <input placeholder="Title"></input>
+      <br></br>
+      <input placeholder="description"></input>
+      {todos.map((oneEntry) => {
+        return (
+          <Todo
+            Title={oneEntry.Title}
+            Description={oneEntry.Description}
+            completed={oneEntry.completed}
+          ></Todo>
+        );
+      })}
+
+      {/* <Todo
+        Title={todos[0].Title}
+        Description={todos[0].Description}
+        completed={todos[0].completed}
+      ></Todo> */}
+    </div>
+  );
+}
+
+// all the hectic game of DOM manipulation that we did earlier was reduced in this code
+
+function Todo(props) {
+  return (
+    <div>
+      <h1>{props.Title}</h1>
+      <h2>{props.Description}</h2>
+      <h3>{props.completed}</h3>
+    </div>
+  );
+}
+
+export default App;
